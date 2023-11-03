09d69b9ea3b4fdb2494a4dcd1f670d389421193a
Amirth24 <amirth300324@gmail.com>
2023-11-03 06:33:47 +0530

    Fixed the bug
    The bug was, the function run_experiments returned reward which is the reward of the last action taken, instead it should return rewards the list of reward obtained during the experiment

--- MultiArmBandits/gradient_bandits.py
+++ MultiArmBandits/gradient_bandits.py
@@ -8,7 +8,6 @@ another is important. No matter how high a preference there is to an action it d
 the probabilty of the action as the probablitity is determined by the softmax distribution.
 """
 from typing import *
-import random
 
 import numpy as np
 from scipy.special import softmax
@@ -18,29 +17,38 @@ from utils import create_reward_distribution, pull_arm, update_distribution
 
 
 def update_preferences(a: int, old_preferences: np.array, reward: float, mean_reward: float, alpha: float) -> np.array:
-    """
+    """Updates the preferences given the reward, mean_reward, and the step size alpha
+
+    Args:
+        a (int): The action that was chosen
+        old_preferences (np.array): the preference that was used to choose the action a
+        reward (float): reward obtained after choosing the action
+        mean_reward (float): the expected reward of the action a
+        alpha (float): the step size
+
+    Returns:
+        np.array: the updated preference 
     """
     proba = softmax(old_preferences)
-    _1 = np.arange(0, len(old_preferences))
-    # new_preferences[a] += alpha * (reward - mean_reward) * (1 - proba[a])
+    new_preferences = old_preferences
+    new_preferences[a] += alpha * (reward - mean_reward) * (1 - proba[a])
 
-    # for i in range(len(old_preferences)):
-    #     if i == a: pass
-    #     new_preferences[i] -= alpha * (reward - mean_reward) * proba[i]
-        
-    # return new_preferences
+    for i in range(len(old_preferences)):
+        if i == a: pass
+        new_preferences[i] -= alpha * (reward - mean_reward) * proba[i]
+    
+    # _1 = np.arange(0, len(old_preferences))
     # print(np.where(_1 == a, 1, 0), a)
 
-    return old_preferences + alpha * (reward - mean_reward) * (np.where(_1 == a, 1, 0) - proba)
+    # return old_preferences + alpha * (reward - mean_reward) * (np.where(_1 == a, 1, 0) - proba)
 
-# def choose_action(preferences: np.array):
-#     proba = softmax(preferences)
-#     return random.choices(list(range(len(preferences))), proba, k=1) [0]
+    return new_preferences
+    
 
 def choose_action(preferences: np.array) -> int:
     return np.argmax(softmax(preferences))
 
-def run_experiments(k_distribution: List[Tuple[float]], steps: int, alpha: float):
+def run_experiments(k_distribution: List[Tuple[float]], steps: int, alpha: float) -> Tuple[List[float], List[float]]:
     """Runs one full experitments with given number of steps
     Args:
         k_distribution: probability distribution for each action
@@ -48,12 +56,10 @@ def run_experiments(k_distribution: List[Tuple[float]], steps: int, alpha: float
         alpha: step size parameter for the updation of preferences 
     Returns:
         rewards: list of reward obtained from each step
-        values_of_action: values of each action calculated by action value method
         max_rewards: list of maximum of means in the distribution
     """
     k = len(k_distribution)
     preferences = np.ones(k) / k
-    # total_reward = [0] * k
     values_of_actions = [0] * k
     rewards = []
     n_of_actions = [0]*k
@@ -62,10 +68,10 @@ def run_experiments(k_distribution: List[Tuple[float]], steps: int, alpha: float
     for i in range(k):
         reward = pull_arm(i, k_distribution)
         rewards.append(reward)
-        # total_reward[i] += reward
+
         n_of_actions[i] += 1
         values_of_actions[i] += (reward - values_of_actions[i]) * 0.9
-        # print(preferences)
+
         preferences = update_preferences(i, preferences, reward, values_of_actions[i], alpha )
 
         max_rewards.append(max(map(lambda x: x[0], k_distribution)))
@@ -75,21 +81,19 @@ def run_experiments(k_distribution: List[Tuple[float]], steps: int, alpha: float
         choice = choose_action(preferences)
         reward = pull_arm(choice, k_distribution)
         rewards.append(reward)
-        # total_reward[i] += reward
         n_of_actions[choice] += 1
         values_of_actions[choice] += (reward - values_of_actions[choice]) * 0.9
 
-        # print(preferences)
         preferences = update_preferences(i, preferences, reward, values_of_actions[choice], alpha )
         max_rewards.append(max(map(lambda x: x[0], k_distribution)))
         k_distribution = update_distribution(k_distribution, 0.001, 0.3)
         
-    return reward, max_rewards
+    return rewards, max_rewards
 
 if __name__ == "__main__":
     K = 10
     k_distribution = create_reward_distribution(K)
-
+    print(k_distribution)
     experiment_configs = [
         (5000,0.01),
         (5000, 0.1),
