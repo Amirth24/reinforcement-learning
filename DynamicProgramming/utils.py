fe51091fd2d55f99c4d7bde0cd70e9ce07029002
Amirthanathan R <amirth300324@gmail.com>
2023-11-06 16:07:06 +0530

    Added Policy Iteration

--- DynamicProgramming/utils.py
+++ DynamicProgramming/utils.py
@@ -1,3 +1,7 @@
+"""
+    Contains all required and important functions to obtain an 
+    optimal policy through dynamic program
+"""
 import gymnasium
 import numpy as np
 
@@ -5,86 +9,64 @@ import numpy as np
 def compute_state_value(
     env: gymnasium.Env,
     policy: np.array,
-    gamma: float = 0.0,
-    max_iter: int = 100,
-    threshold: float = 1e-20,
+    gamma: float = 0.5,
+    threshold: float = 1e-10,
 ) -> np.array:
     """Calculate the state value for the give policy
     Args:
-        env (Env): The The Gymnasium Environment
+        env (Env): The Gymnasium Environment
         policy (np.array): The action distribution for all state.
         gamma (float): The discount factor (default=0.0)
-        max_iter (int): Maximum no of iterations
         threshold (float): Theta value for checking convergence
     Returns:
         np.array: Value of the states
     """
     value_table = np.zeros(env.observation_space.n)
 
-    for i in range(max_iter):
+    # for i in range(max_iter):
+    i= 0
+
+    while True:
         updated_value_table = np.copy(value_table)
-        for st in range(env.observation_space.n):
-            val = []
-            for action in range(env.action_space.n):
-                next_state_reward = []
-                for next_sr in env.unwrapped.P[st][action]:
-                    (
-                        trans_prob,
-                        next_state,
-                        reward_prob,
-                        _,
-                    ) = next_sr
-                    next_state_reward.append(
-                        trans_prob
-                        * (reward_prob + gamma * updated_value_table[next_state])
-                    )
+        for state in range(env.observation_space.n):
+            action = policy[state]
+            value_table[state] = sum([
+                trans_prob * (reward_prob + gamma * updated_value_table[next_state])
+                for trans_prob, next_state, reward_prob, _ in env.unwrapped.P[state][action]
+            ])
 
-                val.append(policy[st, action] * np.sum(next_state_reward))
-
-            value_table[st] = np.sum(val)
-
+        i += 1
         if np.sum(np.fabs(updated_value_table - value_table)) <= threshold:
-            print("Value converged at ", i + 1)
+            print("Value converged at", i)
             break
 
     return value_table
 
-def compute_state_action_value(
-    env: gymnasium.Env,
-    gamma: float = 0.0,
-    max_iter: int = 100,
-    threshold: float = 1e-20,
-):
-    """Calculate the state action value for the give policy
+
+
+def extract_policy(env: gymnasium.Env, value_table: np.array, gamma:float = 0.5) -> np.array:
+    """Extract a better policy for the given value table
     Args:
-        env (Env): The The Gymnasium Environment
-        policy (np.array): The action distribution for all state.
-        gamma (float): The discount factor (default=0.0)
-        max_iter (int): Maximum no of iterations
-        threshold (float): Theta value for checking convergence
+        env (Env): The Gymnasium Environment
+        value_table (np.array): The computed value of states.
+        gamma (float): The discount factor (default=0.5)
     Returns:
-        np.array: Value of the states action pair
+        np.array: Optimal Policy for the given value function
+
     """
-    q_values = np.zeros(
-        (env.observation_space.n, env.action_space.n)
-    )
+    policy = np.zeros(env.observation_space.n)
 
-    for i in range(max_iter):
-        updated_q_values = np.copy(q_values)
-        
-        for state in range(env.observation_space.n):
+    for state in range(env.observation_space.n):
+        q_table = np.zeros(env.action_space.n)
 
-            for action in range(env.action_space.n):
-                action_values = []
-                for next_sr in env.get_wrapper_attr('P')[state][action]:
-                    (
-                        trans_prob,
-                        next_state,
-                        reward,
-                        _
-                    ) = next_sr 
-                    action_values.append(trans_prob * (reward + gamma * np.max(q_values[next_state])))
-                
+        for action in range(env.action_space.n):
+            for next_sr in env.unwrapped.P[state][action]:
+                trans_prob, next_state, reward_prob, _ = next_sr
+                q_table[action] += trans_prob * (reward_prob + gamma * value_table[next_state])
+
+        policy[state] = np.argmax(q_table)
+    return policy
+
 
                 q_values[state, action] = np.sum(action_values)
         
