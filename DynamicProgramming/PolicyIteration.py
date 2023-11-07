7ea127370fb9367c620c9324f36c52128bec4e2a
Amirthanathan R <96509765+Amirth24@users.noreply.github.com>
2023-11-07 14:23:38 +0530

    Add Policy Iteration  (#23)
    * Added state-action value estimation
    
    * Added Policy Iteration
    
    * Created a function for value iteration
    
    * Renamed DynamicProgramming/utils.py to DynamicProgramming/core.py
    
    * Corrected the location of core.py for DP

---
+++ DynamicProgramming/PolicyIteration.py
@@ -0,0 +1,57 @@
+"""
+    Policy Iteration
+    In policy iteration a random policy is initilaized first, then 
+    it will be evaluated by computing the value functions for them. 
+    If it is not a good policy find a better one. Repeat the process
+    until the old policy is same as the new one.
+"""
+import pygame
+import gymnasium as gym
+import numpy as np
+
+from core import compute_state_value, extract_policy
+
+# Creating the environment
+# Changing is_slippery false make the actions deterministic
+env = gym.make(
+    "FrozenLake-v1", 
+    is_slippery=False, 
+    render_mode='human'
+)
+env.reset()
+
+# Creating a random policy
+policy = np.random.randint(0, 4, env.observation_space.n)
+
+print(policy)
+observation = 0
+gamma = 0.8
+for i in range(20000):
+    value = compute_state_value(env, policy, gamma)
+
+    new_policy = extract_policy(env, value, gamma)
+
+    if np.all(policy == new_policy):
+        print("Policy is stablized at", i + 1)
+        break
+
+    policy  = new_policy.astype(int)
+
+print("Optimal Policy", policy)
+
+for i in range(1000):
+    observation, reward, term, trunc, _ = env.step(
+        policy[observation]
+    )
+
+    if trunc or term:
+        print("State Restarted")
+        env.reset()
+
+    env.render()
+
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+
+env.close()
---
+++ DynamicProgramming/core.py
@@ -0,0 +1,92 @@
+"""
+    Contains all required and important functions to obtain an 
+    optimal policy through dynamic program
+"""
+import gymnasium
+import numpy as np
+
+
+def compute_state_value(
+    env: gymnasium.Env,
+    policy: np.array,
+    gamma: float = 0.5,
+    threshold: float = 1e-10,
+) -> np.array:
+    """Calculate the state value for the give policy
+    Args:
+        env (Env): The Gymnasium Environment
+        policy (np.array): The action distribution for all state.
+        gamma (float): The discount factor (default=0.0)
+        threshold (float): Theta value for checking convergence
+    Returns:
+        np.array: Value of the states
+    """
+    value_table = np.zeros(env.observation_space.n)
+
+    # for i in range(max_iter):
+    i= 0
+
+    while True:
+        updated_value_table = np.copy(value_table)
+        for state in range(env.observation_space.n):
+            action = policy[state]
+            value_table[state] = sum([
+                trans_prob * (reward_prob + gamma * updated_value_table[next_state])
+                for trans_prob, next_state, reward_prob, _ in env.unwrapped.P[state][action]
+            ])
+
+        i += 1
+        if np.sum(np.fabs(updated_value_table - value_table)) <= threshold:
+            print("Value converged at", i)
+            break
+
+    return value_table
+
+
+
+def extract_policy(env: gymnasium.Env, value_table: np.array, gamma:float = 0.5) -> np.array:
+    """Extract a better policy for the given value table
+    Args:
+        env (Env): The Gymnasium Environment
+        value_table (np.array): The computed value of states.
+        gamma (float): The discount factor (default=0.5)
+    Returns:
+        np.array: Optimal Policy for the given value function
+
+    """
+    policy = np.zeros(env.observation_space.n)
+
+    for state in range(env.observation_space.n):
+        q_table = np.zeros(env.action_space.n)
+
+        for action in range(env.action_space.n):
+            for next_sr in env.unwrapped.P[state][action]:
+                trans_prob, next_state, reward_prob, _ = next_sr
+                q_table[action] += trans_prob * (reward_prob + gamma * value_table[next_state])
+
+        policy[state] = np.argmax(q_table)
+    return policy
+
+
+def policy_iteration(__env: gymnasium.Env, gamma: float = 0.5, max_iter: int = 20000) -> np.array:
+    """ Finds the optimal policy for the given environement
+    Args:
+        env (Env): The  Gymnasium Environment.
+        gamma (float): The discount factor (default=0.5)
+        max_iter (int) : The maximum no of iteration.
+    Returns
+        np.array : The optimal policy
+    """
+    __policy = np.zeros(__env.observation_space.n)
+    for j in range(max_iter):
+        value = compute_state_value(__env, __policy, gamma)
+
+        new_policy = extract_policy(__env, value, gamma)
+
+        if np.all(__policy == new_policy):
+            print("Policy is stablized at", j + 1)
+            break
+
+        __policy  = new_policy.astype(int)
+
+    return __policy
